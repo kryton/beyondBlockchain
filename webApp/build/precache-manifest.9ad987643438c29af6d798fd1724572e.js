8e94178c9248c374f08e2f280e49bc3306ab42c9
ncolliva <37219786+ncolliva@users.noreply.github.com>
2021-04-05 22:08:00 +0200

    up

---
+++ webApp/build/precache-manifest.9ad987643438c29af6d798fd1724572e.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e066d79dc6ca766dbd02f0cb9fc0c2df",
+    "url": "https://eos.betnow.io/index.html"
+  },
+  {
+    "revision": "bda2745dbc4fe52aa8c2",
+    "url": "https://eos.betnow.io/static/css/main.a0792503.chunk.css"
+  },
+  {
+    "revision": "ecad7fc4a2bea572118b",
+    "url": "https://eos.betnow.io/static/js/2.89af4367.chunk.js"
+  },
+  {
+    "revision": "ee35956434826165d6a317092d6f21eb",
+    "url": "https://eos.betnow.io/static/js/2.89af4367.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bda2745dbc4fe52aa8c2",
+    "url": "https://eos.betnow.io/static/js/main.69890f73.chunk.js"
+  },
+  {
+    "revision": "040cae248b2c31147ae5",
+    "url": "https://eos.betnow.io/static/js/runtime-main.da55734d.js"
+  },
+  {
+    "revision": "c978228391fcc1885fb46be4eb15206d",
+    "url": "https://eos.betnow.io/static/media/BetNowLogoSimple.c9782283.png"
+  },
+  {
+    "revision": "50cf9e39b818257906cfc2f1a610d250",
+    "url": "https://eos.betnow.io/static/media/sidebar.50cf9e39.jpg"
+  }
+]);
